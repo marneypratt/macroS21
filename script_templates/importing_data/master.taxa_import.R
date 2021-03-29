499d9c32316c92229b0e8a992e173d4ff08d2426
Marney Pratt <mcpratt@smith.edu>
2021-03-29 17:59:21 -0400

    upload master taxa and import code

---
+++ script_templates/importing_data/master.taxa_import.R
@@ -0,0 +1,5 @@
+#this will import the list of all taxa for the Mill River and NEON rivers
+
+#make sure you have run library(tideyverse) or library(readr) before running this code
+
+master.taxa <- read_csv("data/neon.mill.taxa.csv")
