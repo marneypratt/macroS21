f977c961a3ad6fb271e25158d93ad99f49c58d0a
Marney Pratt <mcpratt@smith.edu>
2021-04-13 16:44:30 -0400

    minor update

--- script_templates/data_wrangling/density.R
+++ script_templates/data_wrangling/density.R
@@ -3,13 +3,13 @@
 
 # data must also be imported before you can run the code below
 
-macro.density <- mill.macro %>% #replace blank with data frame name
+macro.density <- ___ %>% #replace blank with data frame name
   
   # Sum for each sampleID and the different taxa 
   # remove the organism_aggr variable if you want density of ALL macroinvertebrates
   #replace blank with grouping variables as needed such as season, location, year, etc
-  group_by(sampleID, organism_aggr, location) %>% 
+  group_by(sampleID, organism_aggr, ___) %>% 
   dplyr::summarise (density = sum(invDens, na.rm = TRUE)) %>% 
-  filter(organism_aggr == "Acroneuria") #put the type of organism you want to keep here (remove this line if you want all macros)
+  filter(organism_aggr == "___") #put the type of organism you want to keep here (remove this line if you want all macros)
 
 
