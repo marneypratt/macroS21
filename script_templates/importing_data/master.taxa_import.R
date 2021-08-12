fdd8ce1289709ecf38b5a0689465bc3277db89b8
Marney Pratt <mcpratt@smith.edu>
2021-08-11 23:03:01 -0400

    fixed typo

--- script_templates/importing_data/master.taxa_import.R
+++ script_templates/importing_data/master.taxa_import.R
@@ -1,5 +1,5 @@
 #this will import the list of all taxa for the Mill River and NEON rivers
 
-#make sure you have run library(tideyverse) or library(readr) before running this code
+#make sure you have run library(tidyverse) or library(readr) before running this code
 
 master.taxa <- read_csv("data/neon.mill.taxa.csv")
